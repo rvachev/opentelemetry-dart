d8b1659b2482b6ff454f2929a5f4f725540b4e36
Roman Rvachev <rrvachev@gemsdev.com>
2025-10-02 13:12:36 +0600

    [+] Added proto library to public

---
+++ lib/proto.dart
@@ -0,0 +1,4 @@
+export 'src/sdk/proto/opentelemetry/proto/collector/metrics/v1/metrics_service.pb.dart';
+export 'src/sdk/proto/opentelemetry/proto/common/v1/common.pb.dart';
+export 'src/sdk/proto/opentelemetry/proto/resource/v1/resource.pb.dart';
+export 'src/sdk/proto/opentelemetry/proto/metrics/v1/metrics.pb.dart';
